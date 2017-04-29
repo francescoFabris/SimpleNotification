a9d23ec19048c72928b9a7d67d9899c7ae6e77d5
boemd <davide.boem@studenti.unipd.it>
2017-04-29 19:06:03 +0200

    Inseriti Spinner e array di dati. Modificata anche MainActivity. Fa cagare, ma potrebbe essere sufficiente. Mancano solo le notifiche e il salvataggio dello stato (anche se c'è poco da salvare)

--- app/src/main/java/esp1617/dei/unipd/it/simplenotification/MainActivity.java
+++ app/src/main/java/esp1617/dei/unipd/it/simplenotification/MainActivity.java
@@ -2,12 +2,113 @@ package esp1617.dei.unipd.it.simplenotification;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    int hour;
+    int min;
+    int day;
+    int month;
+    int year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText et = (EditText) findViewById(R.id.note_space);
+        Button bu = (Button) findViewById(R.id.set_notification);
+        Spinner spinner_hour = (Spinner) findViewById(R.id.hour_spinner);
+        Spinner spinner_min = (Spinner) findViewById(R.id.minutes_spinner);
+        Spinner spinner_day = (Spinner) findViewById(R.id.day_spinner);
+        Spinner spinner_month = (Spinner) findViewById(R.id.month_spinner);
+        Spinner spinner_year = (Spinner) findViewById(R.id.year_spinner);
+
+        ArrayAdapter<CharSequence> hour_adapter = ArrayAdapter.createFromResource(this, R.array.hour_array, android.R.layout.simple_spinner_item);
+        hour_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_hour.setAdapter(hour_adapter);
+        spinner_hour.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                hour = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> min_adapter = ArrayAdapter.createFromResource(this, R.array.hour_array, android.R.layout.simple_spinner_item);
+        min_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_min.setAdapter(min_adapter);
+        spinner_min.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                min = position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> day_adapter = ArrayAdapter.createFromResource(this, R.array.day_array, android.R.layout.simple_spinner_item);
+        day_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_day.setAdapter(day_adapter);
+        spinner_day.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                day= position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> month_adapter = ArrayAdapter.createFromResource(this, R.array.month_array, android.R.layout.simple_spinner_item);
+        month_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_month.setAdapter(month_adapter);
+        spinner_month.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                month=position+1;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        ArrayAdapter<CharSequence> year_adapter = ArrayAdapter.createFromResource(this, R.array.years_array, android.R.layout.simple_spinner_item);
+        year_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_year.setAdapter(year_adapter);
+        spinner_year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                year=1+position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        bu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nota = et.getText().toString();
+                //notifica!
+                et.setText("");
+            }
+        });
     }
 }
